bf1a0dad91f9099d4c52fa8f020fb8c3d9ce42bc
Medomale123 <jebiwottmaurine@gmail.com>
2023-08-20 16:49:39 +0300

    added displayWeather function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-let apiKey = "55o434e0bbcb4d5a5a3t61efbb74a386";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={Newyork}&key=${apiKey}";
-console.log(apiUrl);
+let apiKey = "743bee57fddbfaf52447193a87d5dd25";
+let city = "London";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeather);
+function displayWeather(response) {
+  let temperature = document.querySelector(".temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let cityName = document.querySelector("h1");
+  cityName.innerHTML = response.data.name;
+  let humidity = document.querySelector(".humidity");
+  humidity.innerHTML = `Humidity:${response.data.main.humidity}%`;
+  let wind = document.querySelector(".wind");
+  wind.innerHTML = `Wind:${response.data.wind.speed}km/h`;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather.description;
+}
